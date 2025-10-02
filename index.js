f7897f93b03e7e53f6280df4a208e5681d25e49e
init.defaultBranch <kenkar252@gmail.com>
2025-10-02 14:34:04 +0300

    initial

---
+++ index.js
@@ -0,0 +1,22 @@
+let divide = function (a, b) {
+
+  return a /b
+  
+};
+let result = divide(2000,100);
+console.log(result);
+
+const square = (a) =>{
+  return a**2
+
+};
+let res = square(4);
+
+console.log(res)
+
+const add = (a,b) =>{
+  return a +b ;
+};
+
+let addNumbers = add(20,60);
+console.log(addNumbers);
